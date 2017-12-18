8383c0cca7f6623498d7e71f0cc4fa3467000ff7
mevin15 <morgane.evin3@gmail.com>
2017-12-18 14:20:49 +0100

    Date Picker

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,12 +1,16 @@
 package fr.android.androidexercises;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -26,6 +30,19 @@ public class LibraryActivity extends AppCompatActivity {
             intent.putExtra("BOOK", book);
             // TODO add parcel book to intent
             startActivity(intent);
+        });
+
+        Button dateButton = findViewById(R.id.createDatePicker);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(LibraryActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth){
+                        Toast.makeText(LibraryActivity.this, dayOfMonth + "/"+ month+"/"+year,Toast.LENGTH_SHORT).show();
+                    }
+                }, 2017,11,18).show();
+            }
         });
     }
 
